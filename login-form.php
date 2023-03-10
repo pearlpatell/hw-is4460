cab71fa23a5b8cfab4834c306b4ef9ee17a4dfb7
pearlpatell <125989992+pearlpatell@users.noreply.github.com>
2023-03-10 01:19:22 +0100

    Add files via upload

--- login-form.php
+++ login-form.php
@@ -11,7 +11,7 @@
       </ul>
     </nav>
 		<h1> Login </h1>
-		<form action='login.php' method= 'post'>
+		<form action='login-form.php' method= 'post'>
 		<label for='username'> Username: </label>
 		<input type='text' id='username' name='username'>
 		<br>
@@ -22,6 +22,64 @@
 	</form>
 	</body>
 	<p>
-	<a href='user-list.html'> List of users 
+	<a href='user-list.php'> List of users 
 	</p>
 </html>
+
+<?php
+
+require_once 'login.php';
+$conn = new mysqli($hn, $un, $pw, $db);
+if($conn->connect_error) die($conn->connect_error);
+
+if (isset($_POST['username']) && isset($_POST['password'])) {
+	
+	$tmp_username = mysql_entities_fix_string($conn, $_POST['username']);
+	$tmp_password = mysql_entities_fix_string($conn, $_POST['password']);
+	
+	$query = "SELECT password FROM user WHERE username = '$tmp_username'";
+	
+	$result = $conn->query($query); 
+	if(!$result) die($conn->error);
+	
+	$rows = $result->num_rows;
+	$passwordFromDB="";
+	for($j=0; $j<$rows; $j++)
+	{
+		$result->data_seek($j); 
+		$row = $result->fetch_array(MYSQLI_ASSOC);
+		$passwordFromDB = $row['password'];
+	
+	}
+	
+	if(password_verify($tmp_password,$passwordFromDB))
+	{
+		echo "successful login<br>";
+
+		session_start();
+		$_SESSION['username'] = $tmp_username;
+		
+		header("Location: user-add.php");
+	}
+	else
+	{
+		echo "login error<br>";
+	}	
+	
+}
+
+$conn->close();
+
+
+function mysql_entities_fix_string($conn, $string){
+	return htmlentities(mysql_fix_string($conn, $string));	
+}
+
+function mysql_fix_string($conn, $string){
+	$string = stripslashes($string);
+	return $conn->real_escape_string($string);
+}
+
+
+
+?>
